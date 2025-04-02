5d9a021ab0a8362655e0b7835fca39154f13188b
jakkukarthik24 <jakkukarthik24@gmail.com>
2025-04-03 00:13:51 +0530

    Added movie trailer and similar movies to Movie.jsx

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -9,16 +9,40 @@ function Movie(){
     const movie=location.state?.movie;
     if (!movie) return <div className="error">Movie not found.</div>;
     const [similar,setSimilar]=useState([]);
+    const [trailerKey,setTrailerKey]=useState("");
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [movie.id]);
     useEffect(()=>{
-        try{const fetchSimilar=async ()=>{
+        const fetchTrailer=async()=>{
+            try{
+                const response= await fetch(`${BASE_URL}/movie/${movie.id}/videos?api_key=${API_KEY}`);
+                const data=await response.json();
+                const trailer=data.results.find(video=>video.type=="Trailer" && video.site=="YouTube");
+                if(trailer){
+                    setTrailerKey(trailer.key);
+                }
+            }
+            catch(err){
+                console.log("Error finding trailer");
+            }
+        }
+        fetchTrailer()
+    },[movie.id]);
+    useEffect(()=>{
+        const fetchSimilar=async ()=>{
+        try{
+            
             const response=await fetch(`${BASE_URL}/movie/${movie.id}/similar?api_key=${API_KEY}`);
             const data=await response.json();
             setSimilar(data.results.slice(0,6));
-        }}
+        }
         catch(err){
             console.log("error finding similar movies");
         }
-    });
+         }
+        fetchSimilar();
+    },[movie.id]);
     return(
         <>
         <div className="movie-detail">
@@ -32,8 +56,35 @@ function Movie(){
                 <br/>
                 <p ><span className="info">Release Date : </span> {movie.release_date}</p>
                 <p ><span className="info">Vote Average : </span> {movie.vote_average}</p>
+                <br/>
+                {trailerKey ? (
+                    <div className="trailer-container">
+                        <iframe
+                        height="320"
+                        width="560"
+                        src={`https://www.youtube.com/embed/${trailerKey}`}
+                        title={`${movie.title} Trailer`}
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+                    </div>
+                )
+                :(
+                    <p>Trailer Unavailable</p>
+                )}
             </div>
         </div>
+        <div className="similar">
+            <h2>Similar Movies</h2>
+            
+            <div className="similar-movies">
+                    {similar.length > 0 ? (
+                        similar.map(movie => <MovieCard movie={movie} key={movie.id} />)
+                    ) : (
+                        <p>No similar movies found.</p>
+                    )}
+            </div>
+         </div>
         </>
     );
 }
