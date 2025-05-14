ab250fb13a98c63fed3799c560d461f6401afcac
jakkukarthik24 <jakkukarthik24@gmail.com>
2025-05-14 16:55:51 +0530

    Complete deployment

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 app.use(session({
     secret:process.env.SESSION_SECRET,
     resave:false,
