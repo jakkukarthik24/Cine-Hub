26fa6574245f7e77afda4f4738933e6536de230e
jakkukarthik24 <jakkukarthik24@gmail.com>
2025-05-14 13:00:45 +0530

    Ready for deployment

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import db from '../db.js';
