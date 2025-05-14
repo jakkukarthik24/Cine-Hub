3a946509aebde7ce17b7e42cc2717b113b7b65c2
jakkukarthik24 <jakkukarthik24@gmail.com>
2025-05-14 15:17:06 +0530

    Render database

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const { Client } = pkg;
 const pg=new Client({
   connectionString: process.env.DATABASE_URL,
-  
-})
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
 await pg.connect();
 export default pg;
