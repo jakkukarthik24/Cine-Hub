a9fcac9b522c586dac044df48b569ecbc02ab77c
jakkukarthik24 <jakkukarthik24@gmail.com>
2025-09-09 23:14:04 +0530

    Resolved bugs in Movie.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './css/index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './css/index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((reg) => reg.unregister());
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,4 +16,4 @@ createRoot(document.getElementById('root')).render(
       <App />
     </BrowserRouter>
   </StrictMode>
-)
+);
