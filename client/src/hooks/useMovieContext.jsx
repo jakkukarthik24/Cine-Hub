5ea249a0f8a21f4230bb26357a7b3139ae87e506
jakkukarthik24 <jakkukarthik24@gmail.com>
2025-09-12 02:05:36 +0530

    Resolved React error 310

--- client/src/hooks/useMovieContext.jsx
+++ client/src/hooks/useMovieContext.jsx
@@ -1,10 +1,3 @@
 import { useContext } from "react";
-import { MovieContext } from "../context/MovieContext";
-
-export const useMovieContext = () => {
-  const context = useContext(MovieContext);
-  if (!context) {
-    throw new Error("useMovieContext must be used within a MovieProvider");
-  }
-  return context;
-};
+import { MovieContext } from "../contexts/MovieContext";
+export const useMovieContext = () => useContext(MovieContext);
